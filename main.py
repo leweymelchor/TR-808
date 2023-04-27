745c1fe446cd1f2fc84cc8b2ba628478843624ef
Lewey <dlmelchor12@gmail.com>
2023-04-27 13:23:27 -0700

    updated display window
    created.a draw grid function for the display, added text for the 6 instruments, built 2 sections one for instruments and one for the lower menu

--- main.py
+++ main.py
@@ -17,11 +17,32 @@ label_font = pygame.font.Font('freesansbold.ttf', 32)
 FPS = 60
 TIMER = pygame.time.Clock()
 
-run = True
 
+def draw_grid():
+    left_menu = pygame.draw.rect(screen, gray, [0, 0, 200, HEIGHT - 195], 5)
+    lower_menu = pygame.draw.rect(screen, gray, [0, HEIGHT - 200, WIDTH, 200], 5)
+    pads = []
+    colors = [gray, white, gray]
+    kick_text = label_font.render("Kick", True, white)
+    sub_text = label_font.render("808", True, white)
+    snare_text = label_font.render("Snare", True, white)
+    clap_text = label_font.render("Clap", True, white)
+    hi_hat_text = label_font.render("Hi-hat", True, white)
+    crash_text = label_font.render("Crash", True, white)
+
+    screen.blit(kick_text, (30, 30))
+    screen.blit(sub_text, (30, 130))
+    screen.blit(snare_text, (30, 230))
+    screen.blit(clap_text, (30, 330))
+    screen.blit(hi_hat_text, (30, 430))
+    screen.blit(crash_text, (30, 530))
+
+run = True
 while run:
     TIMER.tick(FPS)
     screen.fill(black)
+    draw_grid()
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
